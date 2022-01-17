9b5d533e12fc6523e8e1b986fb68db64173bf492
Gaurav-Surana <90751200+Gaurav-Surana@users.noreply.github.com>
2022-01-17 14:03:05 +0530

    1)updating Main class(PubSubToBQ) 2)updating pom.xml 3)Change data parsing logic

--- src/main/java/CommonLog.java
+++ src/main/java/CommonLog.java
@@ -1,15 +1,15 @@
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 
-/**
- * Creating schema for storing messages receive from Pubsub topic.
- */
+
 @DefaultSchema(JavaFieldSchema.class)
-    public class CommonLog {
-        int id;
-        String name;
-        String surname;
-    }
+public class CommonLog {
+    @javax.annotation.Nullable int id;
+    @javax.annotation.Nullable String name;
+    @javax.annotation.Nullable String surname;
 
-
-
+    // @Override
+    // public String toString() {
+    //     return String.format("{\"id\":%d,\"name\":%s,\"surname\":%s}", id,name,surname);
+    // }
+}
--- src/main/java/PubSubToBQ.java
+++ src/main/java/PubSubToBQ.java
@@ -1,4 +1,3 @@
-//import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import com.google.gson.Gson;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
@@ -7,86 +6,133 @@ import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.transforms.JsonToRow;
 
 public class PubSubToBQ {
 
-    private static TupleTag<CommonLog> VALID_DATA=new TupleTag<CommonLog>(){};
+    private static TupleTag<String> VALID_DATA=new TupleTag<String>(){};
     private static TupleTag<String> INVALID_DATA=new TupleTag<String>(){};
     private static final Logger LOG = LoggerFactory.getLogger(PubSubToBQ.class);
 
 
     /*
-    providing custome execution  option passed by passing parameters through command lines.
-     */
+    Additional requirements 1. -> Pub/Sub topic name, Table name, topic subscription and DLQ topic are parametric.
+    */
     public interface PipelineOptions extends DataflowPipelineOptions {
+        @Description("Input topic name")
+        String getInputTopic();
+        void setInputTopic(String inputTopic);
 
-    }
+        @Description("BigQuery table name")
+        String getTableName();
+        void setTableName(String tableName);
 
-    public static void main(String[] args) {
-        PipelineOptionsFactory.register(PipelineOptions.class);
-        PipelineOptions options = PipelineOptionsFactory.fromArgs(args)
-                .withValidation()
-                .as(PipelineOptions.class);
-//        PipelineOptions options = (PipelineOptions) PipelineOptionsFactory.fromArgs(args).withValidation().create();
+        @Description("input Subscription of PubSub")
+        String getSubscription();
+        void setSubscription(String subscription);
 
-        run(options);
-
-
-
+        @Description("DLQ topic of PubSub")
+        String getDlqTopic();
+        void setDlqTopic(String dlqTopic);
     }
 
     /**
-     * A DoFn acccepting Json and outputing CommonLog with Beam Schema
+     * Execution of main pipeline will be start from here.
      */
-    static class JsonToCommonLog extends DoFn<String, CommonLog> {
+    public static void main(String[] args) {
+        //To provide options at run time we willl use PipelineOptionsFactory registry methode.
+        PipelineOptionsFactory.register(PipelineOptions.class);
+        //with validation will check for each option.
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args)
+                .withValidation()
+                .as(PipelineOptions.class);
+        run(options);
+    }
+
+    /**
+     * Here we will apply lables to our data(VALID AND INVALID ) with the help of TupleTag.
+     */
+    static class JsonToCommonLog extends DoFn<String, String> {
         @ProcessElement
-        public void processElement(@Element String json, OutputReceiver<CommonLog> r,ProcessContext processContext) throws Exception {
-            try {
-                Gson gson = new Gson();
-                CommonLog commonLog = gson.fromJson(json, CommonLog.class);
-//                r.output(commonLog);
-                processContext.output(VALID_DATA,commonLog);
-            }catch(Exception e){
+        public void processElement(@Element String json,ProcessContext processContext) throws Exception {
+            // try {
+            //     Gson gson = new Gson();
+            //     CommonLog commonLog = gson.fromJson(json, CommonLog.class);
+            //     processContext.output(VALID_DATA,commonLog);
+            // }catch(Exception e){
+            //     LOG.info(e.toString());
+            //     processContext.output(INVALID_DATA,json);
+            // }
+            String[] arrJson=json.split(",");
+            if(arrJson.length==3) {
+                //validatios
+                if(arr[0].contains("id") && arr[1].contains("name") &&arr[2].contains("surname")){
+                    processContext.output(VALID_DATA,json);
+                }else{
+                    //Malformed data
+                    processContext.output(INVALID_DATA,json);
+                }
+            }else{
+                //Malformed data
                 processContext.output(INVALID_DATA,json);
             }
         }
     }
 
-    public static PipelineResult run(PipelineOptions options) {
-        String inputTopic="projects/nttdata-c4e-bde/topics/uc1-input-topic-16";
-        String subscriptionData="projects/nttdata-c4e-bde/subscriptions/uc1-input-topic-sub-16";
-        String outputBigQueryTable="nttdata-c4e-bde:uc1_16.account";
-        String dlqTopic="projects/nttdata-c4e-bde/topics/uc1-dlq-topic-16";
 
-        //set job name
+    //building schema
+    //this will help us during transforming data fom string json to Row schema type.
+    public static final Schema rawSchema = Schema
+            .builder()
+            .addInt32Field("id")
+            .addStringField("name")
+            .addStringField("surname")
+            .build();
+
+
+
+    /**
+     * This method will create pipeline and use all options provided during commandline arguments.
+     */
+    public static PipelineResult run(PipelineOptions options) {
+        //read from subscription.
+        String PubSubsubscriptionName="projects/"+options.getProject()+"/subscriptions/"+options.getSubscription();
+        // Bigquery table name.
+        String outputTableName=options.getProject()+":"+options.getTableName();
+        //DLQ topic
+        String dlqTopicName="projects/"+options.getProject()+"/topics/"+options.getDlqTopic();
+        //job Name
         options.setJobName(options.getJobName());
 
-        // Create the pipeline
+        //create pipeline
         Pipeline pipeline = Pipeline.create(options);
-        //read data from pubsub.
-        PCollectionTuple rowCheck = pipeline
-                .apply("ReadMessage", PubsubIO.readStrings()
-                        .fromSubscription(subscriptionData))
 
-                //transform
+        //Read data from pubsub subscription.
+        PCollectionTuple rowCheck = pipeline
+                .apply("ReadMessageFromPubSub", PubsubIO.readStrings()
+                        .fromSubscription(PubSubsubscriptionName))
+
+                //Filter data into two cateogory (VALID and INVALID).
                 .apply("ParseJson", ParDo.of(new JsonToCommonLog()).withOutputTags(VALID_DATA, TupleTagList.of(INVALID_DATA)));
 
-        //Extract valid and invalid data.
-        PCollection<CommonLog> validData=rowCheck.get(VALID_DATA);
+        //get PCollection<String> for both VALID and INVALID Data.
+        PCollection<String> validData=rowCheck.get(VALID_DATA);
         PCollection<String> invalidData=rowCheck.get(INVALID_DATA);
 
-        //load data in BQ.
-        validData.apply("WriteRawToBQ",
-                BigQueryIO.<CommonLog>write().to(outputBigQueryTable).useBeamSchema()
-                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+        //change data from PCollection<String> to Row
+        validData.apply("TransformToRow", JsonToRow.withSchema(rawSchema))
+                .apply("WriteDataToTable",
+                        BigQueryIO.<Row>write().to(outputTableName).useBeamSchema()
+                                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
 
-
-        invalidData.apply(PubsubIO.writeStrings().to(dlqTopic));
+        //write Invalid data(malformed data) to Big query.
+        invalidData.apply("SendInValidDataToDLQ",PubsubIO.writeStrings().to(dlqTopicName));
         LOG.info("Building pipeline...");
         return pipeline.run();
 
